61ca41b75486e33217e3dac1d46ea2c76b535440
aymericgoube <aymericgoube@hotmail.fr>
2017-06-22 15:20:54 +0200

    amélioration de la page actualite

--- view/actualite.php
+++ view/actualite.php
@@ -3,13 +3,12 @@
   <div class="article-container">
     <h1>ACTUS</h1>
     <div class="container">
-      <div class="row">
-
+<div class="row">
         <div class="col-md-3 col-sm-12">
           <div class="col-md-12">
             <img src="img/eiffel.jpg" alt="">
           <h5>TITLE</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+          <p></p>
           <a href="#" class="read-more">Lire plus</a>
             </div>
         </div>
@@ -17,7 +16,7 @@
           <div class="col-md-12">
 <img src="img/eiffel.jpg" alt="">
           <h5>TITLE</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor co laboris nisi ut aliquip ex ea commodo consequat. </p>
             <a href="#" class="read-more">Lire plus</a>
               </div>
         </div>
@@ -34,7 +33,7 @@
             <img src="img/eiffel.jpg" alt="">
 
           <h5>TITLE</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+          <p>Lorem ipsum dolor sit amet, </p>
             <a href="#" class="read-more">Lire plus</a>
         </div>
         </div>
@@ -46,7 +45,32 @@
             <a href="#" class="read-more">Lire plus</a>
               </div>
         </div>
-      </div>
+        <div class="col-md-3 col-sm-12">
+          <div class="col-md-12">
+              <img src="img/eiffel.jpg" alt="">
+          <h5>TITLE</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+            <a href="#" class="read-more">Lire plus</a>
+              </div>
+        </div>
+        <div class="col-md-3 col-sm-12">
+          <div class="col-md-12">
+              <img src="img/eiffel.jpg" alt="">
+          <h5>TITLE</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+            <a href="#" class="read-more">Lire plus</a>
+              </div>
+        </div>
+        <div class="col-md-3 col-sm-12">
+          <div class="col-md-12">
+              <img src="img/eiffel.jpg" alt="">
+          <h5>TITLE</h5>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+            <a href="#" class="read-more">Lire plus</a>
+              </div>
+        </div>
+          </div>
+
     </div>
   </div>
 </body>
